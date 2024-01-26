c4ab8b16cf5237de56221a7f258ab7fe1f91501f
Saravanakumar R <100985347+Saravanakumar2003@users.noreply.github.com>
2024-01-26 21:24:00 +0530

    Audio Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,23 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { HeaderContainer, MobileIcon, NavMenu, Icons } from './styles'
 import { List, X } from 'phosphor-react'
-import { FiGithub, FiLinkedin } from 'react-icons/fi'
+import { FiGithub, FiLinkedin, FiInstagram } from 'react-icons/fi'
 import { FaWhatsapp } from 'react-icons/fa'
 
+import AudioPlayer from '../Music/AudioPlayer';
+
 export function Header() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
+
+  const musicSrc = 'https://preview-downloads.customer.envatousercontent.com/files/311477275/preview.mp3?response-content-disposition=attachment%3Bfilename%3D29263231_at-dubstep_by_zedbadley_preview.mp3&Expires=1706873095&Signature=Z-viT8fTOVEOJA1m-fFnocWAZRwO7qgm2PIo9U5C-pAOP-O3Y6enPh~qdjiwHNFrVRKN3IXpfr1wh0KGauKqFPS6abRmT23c4zBVJTA9mqJIZ5ka9JsWy8R-O0X9wwbZbB2QXhclNGZQE6v2HH62MdtzN2g89D3eHxae9RXj8Oi3bzSBIbgTo2ekprnD5U0rW2k-skQSvYXdrhxF59NPjkEQ3gC-bjfLodvYHs2Iaelsdwh7-mtdguPeHt8eViAjpcJUC7PqDY2LoEX6022FMrdYrYkI0RDfpumKRt~qHQOm5GupeJZRv-gGnXSZDJ6D3CJwB2bs4kBIaEiFZwgl-M3UwJzAf5N8fDZk7QpTOUdGLLabGpYsZEaun24zx1qSdwvGXWgxBNFolERIiQJae6s28BDYIM7h8qA-zq6NDHvkYW1vAitl0g7STsrLvlIsRez-YtI5fyHTn-jlKJ0I-4gIlkPkZ9J4Onfp93qP-yJHA5cBHLOXuXzBcU2nIfnyh7Qe-VViI~vp5J6veQVPPFIMNk2zB1XxrIzKflJbimKKgKMXWspUYUXi0c8PYRz1tFzKFUFL4PgMUqspBWDaHLj-AbH49vUa9Y6YR~BFfnoEC1kRn-uhPy~gDa0NtqDGxM-nKDuhtpLKJeUPUdb5pC80N3tLVAuT8TYahR9D9hQ_&Key-Pair-Id=APKAJRP2AVKNFZOM4BLQ'; 
 
   return (
-    <HeaderContainer style={{ position: 'fixed' }}>
-      <div className="mobile-content">
+    <HeaderContainer>
+      <div className="mobile-content"  style={{ display: 'flex'}}>
         <Link href={'/'}>
           <div className="logo">
             <Image
@@ -28,6 +32,17 @@ export function Header() {
           </div>
           {'Saravanakumar'}
         </Link>
+
+        <div className="audio-player">
+          <AudioPlayer audioSrc={musicSrc} />
+        </div>
+        <style jsx>{`
+        .audio-player {
+          margin: 5px 20px;
+          flex-grow: 1; 
+          max-width: 600px; 
+        }
+          `}</style>
 
         <MobileIcon onClick={handleOpen}>
           {open ? (
@@ -96,6 +111,7 @@ export function Header() {
             </Link>
           </li>
         </ul>
+
         <Icons>
           <Link
             href={'https://github.com/Saravanakumar2003'}
@@ -113,6 +129,16 @@ export function Header() {
             <FiLinkedin />
 
           </Link>
+
+          <Link 
+           href={'https://www.instagram.com/saravanakumar.me?utm_source=qr'}
+           target="_blank" 
+           aria-label="Instagram">
+            
+          <FiInstagram />
+          
+        </Link>
+
           <Link
             href={'https://api.whatsapp.com/send?phone=918838416187'}
             target="_blank"
@@ -121,6 +147,8 @@ export function Header() {
             <FaWhatsapp />
 
           </Link>
+
+
         </Icons>
       </NavMenu>
     </HeaderContainer>
---
+++ src/components/Music/AudioPlayer.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { FaPlay, FaPause } from 'react-icons/fa';
+
+const AudioPlayer: React.FC<{ audioSrc: string }> = ({ audioSrc }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = React.createRef<HTMLAudioElement>();
+
+  const togglePlay = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <audio ref={audioRef} src={audioSrc} style={{ flex: 1 }} />
+      <button 
+        onClick={togglePlay} 
+        style={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            width: '45px', 
+            height: '45px', 
+            borderRadius: '50%', 
+            backgroundColor: 'white', 
+            color: 'black', 
+            cursor: 'pointer'
+        }}
+      >
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+    </div>
+  );
+};
+
+export default AudioPlayer;

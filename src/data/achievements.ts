8db5e88cdeb0407e8b77fa40a39b27c956b969e7
Saravanakumar2003 <saravanaramaswamy2003@gmail.com>
2024-04-08 21:23:28 +0530

    update

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -3,7 +3,7 @@ const Achievements = [
         id: 1,
         name: 'Winners - Velammal Hackathon 1.0',
         description: 'Our Project, EmpowerKids is a gamified platform designed to educate children aged 8 to 16 about their rights in India.',
-        image: ['/achieve/award3.jpg','/achieve/award3_2.jpg','/achieve/award3_3.jpg'],
+        image: ['/achieve/award3.jpg','/achieve/Award3_2.jpg','/achieve/Award3_3.jpg'],
         year: 'April 2024',
         prize: 'Rs.5000 & Certificate',
         url: 'https://drive.google.com/file/d/1DEgYalgGcs5j5ZRcRZ7Wdfnw37ww3b4Z/view?usp=sharing'
@@ -12,7 +12,7 @@ const Achievements = [
         id: 2,
         name: '34th Essay Writing Competition',
         description: 'I have secured third place in the All India Essay Contest conducted by the Department Of Atomic Energy with a cash prize of Rs. 15,000.',
-        image: ['/achieve/award1.png','/achieve/award1_2.jpg','/achieve/award1_3.jpg','/achieve/award1_4.jpg'],
+        image: ['/achieve/award1.png','/achieve/Award1_2.jpg','/achieve/Award1_3.jpg','/achieve/Award1-4.jpg'],
         year: 'October 2022',
         prize:  'Rs.15000 & Certificate',
         url: 'https://drive.google.com/file/d/1xvUpa02LIx7qYksSwhkdhP_xJPRvlnfx/view?usp=sharing'
@@ -21,7 +21,7 @@ const Achievements = [
         id: 3,
         name: 'National Children Science Congress',
         description: 'I won school & district level of NCSC and participated in State level NCSC and bagged certificate, trophy & thulir book of NCSC.',
-        image: ['/achieve/award2.jpg','/achieve/award2_2.jpg','/achieve/award2_3.jpg'],
+        image: ['/achieve/award2.jpg','/achieve/Award2_2.jpg','/achieve/Award2_3.jpg'],
         year: 'Feburary 2021',
         prize: 'Certificate and Shield',
         url: 'https://drive.google.com/file/d/1ZbK0MuuLRjvQWeVqNK7L7OnDAz4lPguK/view?usp=sharing'

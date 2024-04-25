0428774a3357286afa37ac2837af8919749abb26
Saravanakumar R <Saravanaramaswamy2003@gmail.com>
2024-04-25 22:50:55 +0530

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,3 +1,6 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import projects from "../data/projects";
 import { Footer } from "../components/Footer";

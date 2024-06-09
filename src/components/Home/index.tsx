ece2352b2d379500ffb5a15f942766a6120e1175
Saravanakumar2003 <saravanaramaswamy2003@gmail.com>
2024-06-09 16:43:20 +0530

    Minor Update

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,11 +3,30 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Typewriter from 'typewriter-effect'
-import { ButtonPrimary, Container } from '../../styles/styles'
+import { Button, ButtonPrimary, Container } from '../../styles/styles'
 import { Content, ImgHome, HomeText } from './styles'
 import { FiArrowRight } from 'react-icons/fi'
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+
+Modal.setAppElement('#__next');
 
 export function HomeHero() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  function handleContinue() {
+    window.location.href = "https://storyofsaravana.vercel.app/";
+  }
+
   return (
     <Container>
       <Content>
@@ -37,12 +56,90 @@ export function HomeHero() {
           </h2>
 
           <div className="button">
-            <Link href="https://storyofsaravana.vercel.app/">
-              <ButtonPrimary>
-                  <b>See my Story</b>
-                  <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
-              </ButtonPrimary>
-            </Link>
+            <ButtonPrimary onClick={openModal}>
+              <b>See my Story</b>
+              <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
+            </ButtonPrimary>
+
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              contentLabel="Example Modal"
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(0, 0, 0, 0.75)', // semi-transparent black
+                },
+                content: {
+                  alignContent: 'center',
+                  color: 'lightsteelblue',
+                  top: '50%',
+                  left: '50%',
+                  right: 'auto',
+                  bottom: 'auto',
+                  marginRight: '-50%',
+                  transform: 'translate(-50%, -50%)',
+                  backgroundColor: '#fff',
+                  borderRadius: '10px', 
+                  padding: '20px',
+                },
+              }}
+            >
+              <h2 style={{
+                color: 'black',
+                textAlign: 'center',
+                fontSize: '1.5rem',
+              }}>Story of Saravanakumar</h2>
+              <p style={{
+                marginTop: '1rem',
+                color: 'black',
+                textAlign: 'center',
+                fontSize: '1rem',
+              }}>Continue only if you are in computer.</p>
+              <p style={{
+                color: 'black',
+                textAlign: 'center',
+                fontSize: '1rem',
+              }}>If you are on phone the page may not load properly,</p>
+              <p style={{
+                color: 'black',
+                textAlign: 'center',
+                fontSize: '1rem',
+              }}>and several animations may not appear.</p>
+              <p style={{
+                marginTop: '1rem',
+                color: 'black',
+                textAlign: 'center',
+                fontSize: '1rem',
+                fontWeight: 'bold',
+              }}>Do you want to continue?</p>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}>
+              <Button style={{
+                display: 'inline-block',
+                alignContent: 'center',
+                marginTop: '1rem',
+                marginRight: '1rem',
+                backgroundColor: 'red',
+                color: 'white',
+                border: 'none',
+                padding: '0.5rem 1rem',
+                borderRadius: '5px',
+              }} onClick={closeModal}>Close</Button>
+              <Button style={{
+                display: 'inline-block',
+                alignSelf: 'center',
+                marginTop: '1rem',
+                marginRight: '1rem',
+                backgroundColor: 'green',
+                color: 'white',
+                border: 'none',
+                padding: '0.5rem 1rem',
+                borderRadius: '5px',
+              }} onClick={handleContinue}>Continue</Button>
+              </div>
+            </Modal>
           </div>
         </HomeText>
         <ImgHome>
--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -2,7 +2,7 @@ const blog = [
   {
     id: 1,
     title: 'Immersive Insights: Build With AI Workshop by GDSC',
-    url: 'https://saravanakumar2003.hashnode.dev/immersive-insights-build-with-ai-workshop-by-gdsc',
+    url: 'https://blog.saravanakumar.engineer/immersive-insights-build-with-ai-workshop-by-gdsc',
     description: 'The buzz surrounding the Build with AI Roadshow was palpable as tech enthusiasts from across Chennai eagerly awaited the chance to immerse themselves in a day of learning, collaboration, and exploration.',
     img: '/blog/BLOG1.jpg',
     date: '11/05/2024',

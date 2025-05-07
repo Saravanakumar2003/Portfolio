453a60d0aa991ce1dd8105b07be7ccf61f5a6e63
Saravanakumar2003 <saravanaramaswamy2003@gmail.com>
2025-05-08 00:35:20 +0530

    feat: Add html-to-image and react-confetti dependencies; implement card generation feature in HomeHero component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,21 +4,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Typewriter from 'typewriter-effect'
 import { Button, ButtonPrimary, Container } from '../../styles/styles'
-import { Content, ImgHome, HomeText } from './styles'
+import { Content, ImgHome, HomeText, Card, Announcement, Logo, Box } from './styles'
 import { FiArrowRight } from 'react-icons/fi'
 import { useState } from 'react';
 import Modal from 'react-modal';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useRef } from 'react';
+import * as htmlToImage from 'html-to-image';
+import Confetti from 'react-confetti';
 
 Modal.setAppElement('#__next');
 
 export function HomeHero() {
   const { t } = useTranslation('common');
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const router = useRouter();
+  const [modalIsOpen2, setModalIsOpen2] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [date, setDate] = useState('');
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
   const [currentLang, setCurrentLang] = useState<'en' | 'ta'>('en');
+  const router = useRouter();
 
   useEffect(() => {
     const { locale } = router;
@@ -33,6 +42,55 @@ export function HomeHero() {
     setModalIsOpen(false);
   }
 
+  function openModal2() {
+    setModalIsOpen2(true);
+  }
+
+  useEffect(() => {
+    // Fetch the current date using JavaScript's built-in Date object
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    setDate(currentDate);
+  }, []);
+
+  function closeModal2() {
+    setModalIsOpen2(false);
+  }
+
+  function handleGenerate() {
+    if (userName.trim() === '') {
+      alert('Please enter a name!');
+      return;
+    }
+
+    setShowCard(true);
+
+    // Trigger confetti
+    setShowConfetti(true);
+
+    // Stop confetti after 3 seconds
+    setTimeout(() => {
+      setShowConfetti(false);
+    }, 10000);
+  }
+
+  async function downloadCard() {
+    if (cardRef.current) {
+      try {
+        const dataUrl = await htmlToImage.toPng(cardRef.current); // Convert the card to PNG
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'business-card.png'; // Set the download filename
+        link.click(); // Trigger the download
+      } catch (error) {
+        console.error('Failed to generate the business card image:', error);
+      }
+    }
+  }
+
   function handleContinue() {
     window.location.href = "https://storyofsaravana.vercel.app/";
   }
@@ -45,7 +103,7 @@ export function HomeHero() {
             <span>👋🏻</span> {currentLang === 'ta' ? 'வணக்கம், எனது பெயர்' : 'Hello, my name is'}
           </p>
           <h1>
-            {currentLang === 'ta' ? 'சரவணகுமார் ரா' : 'Saravanakumar R'}  
+            {currentLang === 'ta' ? 'சரவணகுமார் ரா' : 'Saravanakumar R'}
             <span className="animation">
               <Image
                 width={200}
@@ -68,14 +126,14 @@ export function HomeHero() {
 
           <div className="button">
             <ButtonPrimary onClick={openModal}>
-              <b>{currentLang === 'ta' ? 'எனது கதையைப் பார்க்கவும்' : 'See my Story'}</b>
+              <b>{currentLang === 'ta' ? 'எனது கதையைப் பார்க்கவும்' : 'See my story'}</b>
               <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
             </ButtonPrimary>
 
             <Modal
               isOpen={modalIsOpen}
               onRequestClose={closeModal}
-              contentLabel="Example Modal"
+              contentLabel="Info Modal"
               style={{
                 overlay: {
                   backgroundColor: 'rgba(0, 0, 0, 0.75)', // semi-transparent black
@@ -90,7 +148,7 @@ export function HomeHero() {
                   marginRight: '-50%',
                   transform: 'translate(-50%, -50%)',
                   backgroundColor: '#fff',
-                  borderRadius: '10px', 
+                  borderRadius: '10px',
                   padding: '20px',
                 },
               }}
@@ -123,7 +181,7 @@ export function HomeHero() {
                 fontSize: '1rem',
                 fontWeight: 'bold',
               }}>{currentLang === 'ta' ? 'நீங்கள் தொடர விரும்புகிறீர்களா?' : 'Do you want to continue?'}</p> */}
-                            <p style={{
+              <p style={{
                 marginTop: '1rem',
                 color: 'black',
                 textAlign: 'center',
@@ -134,18 +192,18 @@ export function HomeHero() {
                 display: 'flex',
                 justifyContent: 'center',
               }}>
-              <Button style={{
-                display: 'inline-block',
-                alignContent: 'center',
-                marginTop: '1rem',
-                marginRight: '1rem',
-                backgroundColor: 'red',
-                color: 'white',
-                border: 'none',
-                padding: '0.5rem 1rem',
-                borderRadius: '5px',
-              }} onClick={closeModal}>{currentLang === 'ta' ? 'மூடு' : 'Close'}</Button>
-              {/* <Button style={{
+                <Button style={{
+                  display: 'inline-block',
+                  alignContent: 'center',
+                  marginTop: '1rem',
+                  marginRight: '1rem',
+                  backgroundColor: 'red',
+                  color: 'white',
+                  border: 'none',
+                  padding: '0.5rem 1rem',
+                  borderRadius: '5px',
+                }} onClick={closeModal}>{currentLang === 'ta' ? 'மூடு' : 'Close'}</Button>
+                {/* <Button style={{
                 display: 'inline-block',
                 alignSelf: 'center',
                 marginTop: '1rem',
@@ -158,22 +216,160 @@ export function HomeHero() {
               }} onClick={handleContinue}>{currentLang === 'ta' ? 'தொடரவும்' : 'Continue'}</Button> */}
               </div>
             </Modal>
+
+            <ButtonPrimary onClick={openModal2}>
+              <b>{currentLang === 'ta' ? 'அட்டை உருவாக்கவும்' : 'Generate your card'}</b>
+              <FiArrowRight style={{ marginBottom: '-0.3rem' }} size={20} />
+            </ButtonPrimary>
+
+            <Modal
+              isOpen={modalIsOpen2}
+              onRequestClose={closeModal2}
+              contentLabel="Card Modal"
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                },
+                content: {
+                  top: '50%',
+                  left: '50%',
+                  right: 'auto',
+                  bottom: 'auto',
+                  marginRight: '-50%',
+                  transform: 'translate(-50%, -50%)',
+                  backgroundColor: '#fff',
+                  borderRadius: '10px',
+                  padding: '20px',
+                  width: '90%', // Adjust width for responsiveness
+                  maxWidth: '600px', // Limit the maximum width
+                  height: 'auto',
+                },
+              }}
+            >
+              <h2 style={{ textAlign: 'center', color: 'black' }}>
+                {currentLang === 'ta' ? 'அட்டை உருவாக்கவும்' : 'Generate Card'}
+              </h2>
+              <input
+                type="text"
+                placeholder={currentLang === 'ta' ? 'உங்கள் பெயரை உள்ளிடவும்' : 'Enter your name'}
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                style={{
+                  width: '100%',
+                  padding: '10px',
+                  margin: '10px 0',
+                  borderRadius: '5px',
+                  border: '1px solid #ccc',
+                }}
+              />
+              <Button
+                onClick={handleGenerate}
+                style={{
+                  marginBottom: '10px',
+                  backgroundColor: 'blue',
+                  padding: '10px 20px',
+                  borderRadius: '5px',
+                  color: 'white',
+                  margin: '0 auto',
+                }}
+              >
+                Generate
+              </Button>
+              <div
+                ref={cardRef}
+                style={{
+                  width: '100%',
+                  maxWidth: '600px',
+                  height: 'auto',
+                  aspectRatio: '3 / 2',
+                  margin: '20px auto',
+                  padding: '20px',
+                  border: '1px solid #000',
+                  borderRadius: '10px',
+                  textAlign: 'center',
+                  backgroundColor: '#f9f9f9',
+                }}
+              >
+                {showCard && (
+                  <Box>
+                    <Card>
+                      <header>
+                        <span>{date || 'Loading...'}</span>
+                        <Logo>
+                          <Image
+                            width={50}
+                            height={50}
+                            src="/Logo1.png"
+                            alt="logo"
+                            loading="lazy"
+                          />
+                        </Logo>
+                        <span>DEV CARD</span>
+                      </header>
+                      <Announcement>
+                        <h3>Yo! </h3>
+                        <h1>{userName || 'Error'}</h1>
+                        <h3 className="italic">thanks for stopping by my world</h3>
+                      </Announcement>
+                      <span>www.saravanakumar.engineer</span>
+                    </Card>
+                  </Box>
+                )}
+              </div>
+              <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'row' }}>
+                <Button onClick={downloadCard} style={{
+                  marginBottom: '10px',
+                  backgroundColor: 'blue',
+                  padding: '10px 20px',
+                  borderRadius: '5px',
+                  margin: '0 auto',
+                  color: 'white',
+                }}>
+                  {currentLang === 'ta' ? 'பதிவிறக்கவும்' : 'Download'}
+                </Button>
+                <Button
+                  onClick={closeModal2}
+                  style={{
+                    marginTop: '10px',
+                    paddingTop: '10px',
+                    backgroundColor: 'red',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    margin: '0 auto',
+                    color: 'white',
+                  }}
+                >
+                  {currentLang === 'ta' ? 'மூடு' : 'Close'}
+                </Button>
+              </div>
+            </Modal>
           </div>
         </HomeText>
+
+        {showConfetti && (
+          <Confetti
+            width={window.innerWidth}
+            height={window.innerHeight}
+            numberOfPieces={250} 
+            gravity={0.5}
+            wind={0.05}
+          />
+        )}
         <ImgHome>
           <img className="home-img" src="/home.png" alt={currentLang === 'ta' ? 'முகப்பு படம்' : 'Image Home'} />
 
           <div className="code">
             <Image
               width={150}
-              height={150}
+              height={50}
               src="/vectors/dino.svg"
               alt="dino"
               loading="lazy"
             />
           </div>
         </ImgHome>
-      </Content>
-    </Container>
+
+      </Content >
+    </Container >
   )
 }
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,4 +1,184 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+
+export const Box = styled.div`
+  display: flex;
+  text-transform: uppercase;
+  font-family: 'Asap Condensed', sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  .italic {
+    font-style: italic;
+  }
+
+    h1 {
+    font-size: 40px;
+
+    @media (max-width: 768px) {
+      font-size: 30px;
+    }
+    @media (max-width: 500px) {
+      font-size: 20px;
+    }
+  }
+
+    h3 {
+      font-size: 20px;
+
+      font-weight: 400;
+      @media (max-width: 768px) {
+        font-size: 15px;
+      }
+
+      @media (max-width: 500px) {
+        font-size: 12px;
+      }
+    }
+
+     span {
+      font-size: 20px;
+      text-align: center;
+      left: 5px;
+      right: 5px;
+
+      @media (max-width: 768px) {
+        font-size: 15px;
+      }
+      @media (max-width: 500px) {
+        font-size: 10px;
+      }
+}
+
+`;
+
+export const Card = styled.div`
+  position: relative;
+  margin: auto;
+  height: 350px;
+  width: 600px;
+  text-align: center;
+  background: linear-gradient(#e96874, #6e3663, #2b0830);
+  border-radius: 5px;
+  box-shadow: 0 6px 12px -3px rgba(0, 0, 0, 0.3);
+  color: #fff;
+  padding: 30px;
+
+    @media (max-width: 994px) {
+    width: 100%;
+    height: 300px;
+    padding: 20px;
+  }
+
+    @media (max-width: 500px) {
+    width: 100%;
+    height: 250px;
+    padding: 20px;
+  }
+
+  header {
+    position: absolute;
+    top: 31px;
+    left: 0;
+    width: 100%;
+    padding: 0 10% 0;
+    margin: 0 auto;
+    transform: translateY(-50%);
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+
+    > *:first-child {
+      text-align: left;
+    }
+
+    > *:last-child {
+      text-align: right;
+    }
+    @media (max-width: 994px) {
+      top: 25px;
+    }
+
+    @media (max-width: 500px) {
+      top: 22px;
+    }
+  }
+`;
+
+export const Logo = styled.div`
+  font-size: 24px;
+  position: relative;
+
+  :before,
+  :after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    border-top: 3px solid currentColor;
+    transform: translateY(-50%);
+    
+  }
+
+  :before {
+    right: 120px;
+    width: 30%;
+
+    @media (max-width: 500px) {
+      right: 80px;
+      width: 30%;
+    }
+    
+  }
+
+  :after {
+    left: 120px;
+    width: 30%;
+
+    @media (max-width: 500px) {
+      left: 80px;
+      width: 30%;
+    }
+    
+  }
+
+  span {
+    display: block;
+    position: absolute;
+    width: 100%;
+    top: calc(50% - 1px);
+  }
+`;
+
+export const Announcement = styled.div`
+  position: relative;
+  border: 3px solid currentColor;
+  border-top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  :before,
+  :after {
+    content: '';
+    position: absolute;
+    top: 0px;
+    border-top: 3px solid currentColor;
+    height: 0;
+    width: 15px;
+  }
+
+  :before {
+    left: -3px;
+  }
+
+  :after {
+    right: -3px;
+  }
+`;
 
 export const Content = styled.div`
   display: flex;
